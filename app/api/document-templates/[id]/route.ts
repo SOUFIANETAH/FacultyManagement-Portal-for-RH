22e224994ccf007d5a0c8a42ffb26e1bf95088fc
Soufiane_Tahiri <153372284+SOUFIANETAH@users.noreply.github.com>
2025-06-10 03:05:18 +0100

    Update route.ts

--- app/api/document-templates/[id]/route.ts
+++ app/api/document-templates/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-
+export const dynamic = "force-dynamic";
 const prisma = new PrismaClient();
 // @ts-ignore
 export async function DELETE(
