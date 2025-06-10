497e5ff8b4e4b08bb22d2a8da4e9fce13ae61d11
Soufiane_Tahiri <153372284+SOUFIANETAH@users.noreply.github.com>
2025-06-10 03:12:05 +0100

    Update route.ts

--- app/api/search/students/route.ts
+++ app/api/search/students/route.ts
@@ -1,7 +1,7 @@
 // app/api/search/students/route.ts
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-
+export const dynamic = "force-dynamic";
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
