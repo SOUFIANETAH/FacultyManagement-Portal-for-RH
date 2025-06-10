72de023aa2f79a59e3d32a493e16bb3f60974d57
Soufiane_Tahiri <153372284+SOUFIANETAH@users.noreply.github.com>
2025-06-10 03:07:13 +0100

    Update route.ts

--- app/api/filieres/[codef]/route.ts
+++ app/api/filieres/[codef]/route.ts
@@ -1,7 +1,7 @@
 // app/api/filieres/[codef]/route.ts
 import { NextResponse } from 'next/server';
 import prisma from "../../../lib/db";
-
+export const dynamic = "force-dynamic";
 interface Params {
     params: {
         codef: string;
